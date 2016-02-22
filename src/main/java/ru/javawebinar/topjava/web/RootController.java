455238157454a23fcb7da721c27e05c803340b37
Thetitr <genya_g777@ukr.net>
2016-02-22 20:21:01 +0200

    HW_6

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -6,9 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.LoggedUser;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.UserMealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * User: gkislin
@@ -16,8 +21,11 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class RootController {
+
     @Autowired
     private UserService service;
+    @Autowired
+    private UserMealService mealService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
@@ -36,4 +44,34 @@ public class RootController {
         LoggedUser.setId(userId);
         return "redirect:meals";
     }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String getMeals(HttpServletRequest request) {
+        int userId = LoggedUser.id();
+            request.setAttribute("mealList", UserMealsUtil.getWithExceeded(mealService.getAll(userId), LoggedUser.getCaloriesPerDay()));
+            return "mealList";
+        } else if (action.equals("delete")) {
+            int id = getId(request);
+            LOG.info("Delete {}", id);
+            mealController.delete(id);
+            response.sendRedirect("meals");
+        } else {
+            final UserMeal meal = action.equals("create") ?
+                    new UserMeal(LocalDateTime.now(), "", 1000) :
+                    mealController.get(getId(request));
+            request.setAttribute("meal", meal);
+            request.getRequestDispatcher("mealEdit.jsp").forward(request, response);
+        }
+    }
+
+    @RequestMapping(value = "/meals/delete", method = RequestMethod.GET)
+    public String deleteMeal(HttpServletRequest request) {
+        int userId = LoggedUser.id();
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        mealService.delete(Integer.valueOf(paramId), userId);
+        return "mealList";
+
+
+
+
 }
