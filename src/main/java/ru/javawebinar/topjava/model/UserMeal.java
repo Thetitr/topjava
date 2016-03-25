de5bf938c5cb5e823683ef9fb11eed892db78fe3
boklag <e.boklag@gms-worldwide.com>
2016-03-25 13:34:55 +0200

    9.11

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -24,7 +24,7 @@ import java.time.LocalDateTime;
 })
 @Entity
 @Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")})
-public class UserMeal extends BaseEntity {
+public class UserMeal extends BaseEntity  {
     public static final String GET = "UserMeal.get";
     public static final String ALL_SORTED = "UserMeal.getAll";
     public static final String DELETE = "UserMeal.delete";
